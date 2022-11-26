b617e1416cea96875614deb8e678e09b595932e1
Sumeth4578 <th.sumeth_st@tni.ac.th>
2022-11-26 11:20:19 +0700

    WorkshopA-Week3

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+ // res.send('แมว');
+ res.status(200).json({
+  fullname:'Sumeth Thanadilok'
+})
 });
+
+router.get('/bio', function(req, res, next) {
+  // res.send('แมว');
+  res.status(200).json({
+   fullname:'Sumeth Thanadilok',
+   nickname:'Touch',
+   hobby:'Sleep',
+   gitusername:'Sumeth4578'
+ })
+ });
 
 module.exports = router;
