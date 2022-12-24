68d94b11d3858404bf02e24020344a67a93afeef
Sumeth4578 <th.sumeth_st@tni.ac.th>
2022-12-24 09:49:05 +0700

    WorkshopA

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var companyRoutter = require('./routes/company')
 var staffRoutter = require('./routes/staff')
+var shopRoutter = require('./routes/shop')
 
 var app = express();
 mongoose.connect('mongodb+srv://sumeth:0U4awnrAu1qCy1a2@1913210264-touch.wy7zzms.mongodb.net/restfulapi?retryWrites=true&w=majority' ,
@@ -23,5 +24,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/company',companyRoutter);
 app.use('/staff',staffRoutter )
+app.use('/shop',shopRoutter )
 
 module.exports = app;
---
+++ models/shop.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const staffSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    photo: { type: String, default: "nopic.png" },
+    location: {
+      lat: Number,
+      lgn: Number,
+    },
+    // createdAt: { type: Date, default: Date.now },
+    // updatedAt: { type: Date, default: Date.now },
+  },
+  { collection: "shops", timestamps: true }
+);
+
+const shop = mongoose.model("Shop", staffSchema);
+
+module.exports = shop;
