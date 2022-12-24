efdcf9758bb1e55b674fa3a843a430799c010d89
Sumeth4578 <th.sumeth_st@tni.ac.th>
2022-12-24 10:54:09 +0700

    WorkshopB

--- models/shop.js
+++ models/shop.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const staffSchema = new Schema(
+const shopSchema = new Schema(
   {
     name: { type: String, required: true, trim: true },
     photo: { type: String, default: "nopic.png" },
@@ -15,6 +15,6 @@ const staffSchema = new Schema(
   { collection: "shops", timestamps: true }
 );
 
-const shop = mongoose.model("Shop", staffSchema);
+const shop = mongoose.model("Shop", shopSchema);
 
 module.exports = shop;
