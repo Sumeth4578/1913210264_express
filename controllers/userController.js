8e8fa4d92f258f6c4ce6c606b2e20d4d5b42c493
Sumeth4578 <th.sumeth_st@tni.ac.th>
2023-01-21 11:38:57 +0700

    WorkshopB Week8

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("../config/index");
+
 exports.index = (req, res, next) => {
   res.status(200).json({
     fullname: "Sumeth Thanadilok",
@@ -46,6 +49,54 @@ exports.register = async (req, res, next) => {
 
     res.status(201).json({
       message: "ลงทะเบียนเรียบร้อย",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error("ข้อมูลที่ได้รับมาไม่ถูกต้อง");
+      error.statusCode = 422;
+      error.validation = errors.array();
+      throw error;
+    }
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error("ไม่พบผู้ใช้งาน");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isValid = await user.checkPassword(password);
+    if (!isValid) {
+      const error = new Error("รหัสผ่านไม่ถูกต้อง");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    //create token
+    const token = await jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      config.TOKEN,
+      { expiresIn: "5 days" }
+    );
+
+    const expire_in = jwt.decode(token);
+
+    res.status(200).json({
+      access_token: token,
+      expire_in: expire_in.exp,
+      token_type: 'Bearer',
     });
   } catch (error) {
     next(error);
--- routes/shop.js
+++ routes/shop.js
@@ -1,18 +1,31 @@
 var express = require("express");
 var router = express.Router();
 const shopController = require("../controllers/shopController");
-const { body} = require('express-validator');
+const { body } = require("express-validator");
 
 router.get("/", shopController.index);
 router.get("/menu", shopController.menu);
 router.get("/:id", shopController.shopMenu);
 
-router.post("/",[
+router.post(
+  "/",
+  [
     body("name").not().isEmpty().withMessage("กรุณาป้อนข้อมูล"),
     body("location").not().isEmpty().withMessage("กรุณาป้อนข้อมูล"),
-    body("location.lat").not().isEmpty().withMessage("กรุณาป้อนข้อมูล").isNumeric().withMessage("กรุณาป้อนข้อมูล"),
-    body("location.lgn").not().isEmpty().withMessage("กรุณาป้อนข้อมูล").isNumeric().withMessage("กรุณาป้อนข้อมูล")
-], shopController.insert)
-
+    body("location.lat")
+      .not()
+      .isEmpty()
+      .withMessage("กรุณาป้อนข้อมูล")
+      .isNumeric()
+      .withMessage("กรุณาป้อนข้อมูลเป็นตัวเลข"),
+    body("location.lgn")
+      .not()
+      .isEmpty()
+      .withMessage("กรุณาป้อนข้อมูล")
+      .isNumeric()
+      .withMessage("กรุณาป้อนข้อมูลเป็นตัวเลข"),
+  ],
+  shopController.insert
+);
 
 module.exports = router;
